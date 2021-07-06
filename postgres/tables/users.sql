28f12f1a7bfc131767df96ce602db56f566dabb9
Allen Sarmiento <allensarmiento3@gmail.com>
2021-07-05 17:58:00 -0700

    Add 2 users

--- postgres/tables/users.sql
+++ postgres/tables/users.sql
@@ -10,9 +10,17 @@ CREATE TABLE users (
 
 INSERT INTO users (email, password, role, created)
 VALUES (
-  'test@test.com', 
+  'teacher@test.com', 
   '2d588d736825a13566beb5639d32296a27175ca7db735badf06f0c2d62ae0666fa4e9b23e56208a6142fc68d5939b0f0f46dfb46e346e28a8e333f1c50379419.9669237953263153',
   'teacher',
+  '2021-07-04T08:50:26.000Z'
+);
+
+INSERT INTO users (email, password, role, created)
+VALUES (
+  'student@test.com', 
+  '2d588d736825a13566beb5639d32296a27175ca7db735badf06f0c2d62ae0666fa4e9b23e56208a6142fc68d5939b0f0f46dfb46e346e28a8e333f1c50379419.9669237953263153',
+  'student',
   '2021-07-04T08:50:26.000Z'
 );
 
